705f401238a4a0392088fd78d9e0fe71c26b565e
finntroll_alex <frodo8787@mail.ru>
2018-11-28 00:28:12 +0300

    сделал модульные тесты сделал валидацию полей ID и Name

---
+++ MailSender/Services/NameValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfTestMailSender.Services
+{
+    public class NameValidation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is string str)
+            {
+                if (str == String.Empty) return new ValidationResult(false, "Имя не должно быть пустым");
+                if (str.Length < 3) return new ValidationResult(false,"По правилам - имя длиннее 3 символов");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
